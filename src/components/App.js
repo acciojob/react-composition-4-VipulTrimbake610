9b975a4ddb6160a80bd47be067221e9f4c31a84c
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-22 19:32:57 +0530

    submitting code-Tue Aug 22 2023 19:32:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  let [values, setValues] = useState({name:"",email:"",password:""});
   return (
     <div>
         {/* Do not remove the main div */}
+        <form action="">
+          <label htmlFor="">Name</label>
+          <input type="text" onChange={(e)=>setValues({...values, name:e.target.value})} />
+          <label htmlFor="">Email</label>
+          <input type="email"  onChange={(e)=>setValues({...values, email:e.target.value})}/>
+          <label htmlFor="">Password</label>
+          <input type="password"  onChange={(e)=>setValues({...values, password:e.target.value})}/>
+
+          <button onClick={(e)=>{e.preventDefault();console.log(values);}}>Submit</button>
+        </form>
     </div>
   )
 }
